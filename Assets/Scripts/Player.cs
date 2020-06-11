119a7cd688796bcaabc58848ac973564da24c800
Isfand <isfandyar.ali@me.com>
2020-06-11 13:46:59 +0100

    Implemented a basic cross platform input using Unity own Input layer, need to move ship frame rate independently

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("metres per second")]
+    [SerializeField]
+    private float _xSpeed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffsetThisFrame = xThrow * _xSpeed * Time.deltaTime;
+        Debug.Log(xOffsetThisFrame);
+    }
+}
