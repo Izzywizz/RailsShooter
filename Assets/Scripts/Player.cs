367099d490027497831a80833df8016dbc40e8f1
Isfand <isfandyar.ali@me.com>
2020-06-16 13:52:08 +0100

    The ship is now able to rotate the correct pitch, yaw and roll with design levers to adjust the dramatic turning

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -6,30 +7,83 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    [Header("Speed, Clamp and Position/ Control Pitch")]
+
     [Tooltip("metres per second")]
     [SerializeField]
-    private float _xSpeed = 4.0f;
+    private float _speed = 20.0f;
 
-    [Tooltip("X Clamp Value")]
+    [Tooltip("The Min/ Max value the ship is contrained to in metres")]
     [SerializeField]
-    [Range(0.0f, 4.0f)]
-    private float _xClampValue = 4.0f;
+    [Range(0.0f, 5.0f)]
+    private float _xClampValue = 5.0f;
 
+    [Tooltip("The Min/ Max value the ship is contrained to in metres")]
+    [SerializeField]
+    [Range(0.0f, 3.0f)]
+    private float _yClampedValue = 3.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float _positionPitchFactor = -5.0f;
 
-    // Update is called once per frame
+    [SerializeField]
+    private float _controlPitchFactor = -20.0f;
+
+    [SerializeField]
+    private float _positionYawFactor = 5.0f;
+
+    [SerializeField]
+    private float _controlRollFactor = -20.0f;
+
+    private float _xThrow = 0.0f;
+    private float _yThrow = 0.0f;
+
+    /// Update is called once per frame
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffset = xThrow * _xSpeed * Time.deltaTime;
-        float rawXPos = transform.localPosition.x + xOffset;
-        float clampedRawXPos = Mathf.Clamp(transform.localPosition.x + xOffset, -_xClampValue, _xClampValue);
+        ProcessTranslation();
+        ProcessRotation();
+    }
 
-        transform.localPosition = new Vector3(clampedRawXPos, transform.localPosition.y, transform.localPosition.z);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void ProcessTranslation()
+    {
+        // X - Direction
+        _xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = _xThrow * _speed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float xClampedRawPos = Mathf.Clamp(rawXPos, -_xClampValue, _xClampValue);
+
+        // Y - Direction Movement
+        _yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = _yThrow * _speed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float yClampedRawPos = Mathf.Clamp(rawYPos, -_yClampedValue, _yClampedValue);
+
+        transform.localPosition = new Vector3(xClampedRawPos, yClampedRawPos, transform.localPosition.z);
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void ProcessRotation()
+    {
+        // Rotation - x, y, z
+        // Rotation - pitch, yaw, roll
+        // Order matters with rotation
+
+        float pitchDuetoPosition = transform.localPosition.y * _positionPitchFactor;
+        float pitchDueToControlThrow = _yThrow * _controlPitchFactor;
+        float pitch = pitchDuetoPosition + pitchDueToControlThrow;
+
+        float yaw = transform.localPosition.x * _positionYawFactor;
+
+        float roll = _xThrow * _controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
