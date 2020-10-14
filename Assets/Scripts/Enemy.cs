01e746d8c8b31d57c3e775ca0faf9fd2f493b2ce
Isfand <isfandyar.ali@me.com>
2020-10-14 19:16:46 +0100

    bullet particles can now destroy enemies using collider messaging on the particles

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("Particles collided with enemy " + gameObject.name);
+        Destroy(gameObject);
+    }
+}
